dc18887a561f2eb3194fa6e7cd7e3e921e019d91
wangbingshuai <274694451@qq.com>
2020-02-27 19:12:27 +0800

    定时发送邮件项目demo初始化

---
+++ src/main/java/com/wbs/mailtiming/MailTimingApplication.java
@@ -0,0 +1,15 @@
+package com.wbs.mailtiming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class MailTimingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MailTimingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wbs/mailtiming/MailTimingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wbs.mailtiming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailTimingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
